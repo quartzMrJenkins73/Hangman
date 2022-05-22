520e98279621c5dbc287aa3bbee52a40216b154f
Ashergreen82 <greena@rogers.com>
2022-05-22 17:28:39 -0400

    I converted the code over to OOP for practice.  I also added more to the comments to explain as a historical reminder some of the things I did to prepare for creation of this program.

--- Hangman.py
+++ Hangman.py
@@ -1,56 +1,48 @@
-'''Hangman game'''
+'''Hangman game
+The exercise suggested that I use the "sowpods" list of over 200,000 scrabble words compiled by Peter Norvig.  So I did.
+There are probably a dozen or so ways to do this, but I chose to reformat the word list by putting a number on each line
+and then this made it easy to convert into a dictionary which I then made it easy to pick a random word by choosing a
+random number.  I used this code to format the sowpods file to a new one called sowpods3:
+for index, line in enumerate(wordlist):
+    print("{} {}".format(index, line.strip()))
+    with open("sowpods3.txt", "a") as sowpods:
+        sowpods.write("{} {}".format(index, line.strip()))
+        sowpods.write("\n")
+
+So what the above code does is take each line and using the enumrate function I was able to assign an index to each line
+and then write to the file, effectively giving me a list of words and their corresponding words.  Using the "with"
+operator allows me to open and automatically close the file without having to worry about remembering to use the close()
+function.
+'''
 
 #Step one is to open the file that contains a list of over 200000 words.
 
 import random
 
-# word_list=open("sowpods.txt", "r")
-def create_wordlist():
-    dictionary = {}
-    with open("sowpods3.txt", "r") as wordlist:
-        for line in wordlist:
-            (key, value) = line.split()
+class WordList:
 
-            dictionary[int(key)] = value
-    return dictionary
+    def __init__(self):
+        dictionary = {}
+        with open("sowpods3.txt", "r") as wordlist:
+            for line in wordlist:
+                (key, value) = line.split()
+                dictionary[int(key)] = value
 
-def get_random_word():
-    random_index = random.randint(0, len(word_list))
-    random_word = word_list[random_index]
-    print (f"The random word is {random_word}")
-    return word_list[random_index]
+class RandomWord:
 
-word_list = create_wordlist()
-print(word_list)
-print(len(word_list))
-random_word=get_random_word()
-print(random_word)
-print(get_random_word())
-        # print('\ntext file to dictionary=\n', dictionary)
-# lines = open('sowpods.txt').read().splitlines()  # creates a list with one line per iteù
-# randomLine = random.choice(lines)
-# print(lines)
+    def __init__(self):
+        random_index = random.randint(0, len(self.word_list))
+        self.random_word = self.word_list[random_index]
+        print(f"The random word is {self.random_word}")
 
-    # lines = open('file.txt').read().splitlines()  # creates a list with one line per item
-    # randomLine = random.choice(lines)  # pick up a random item in this list
-    # for index, line in enumerate(wordlist):
-    #     print("{} {}".format(index, line.strip()))
-    #     with open("sowpods3.txt", "a") as sowpods:
-    #         sowpods.write("{} {}".format(index, line.strip()))
-    #         sowpods.write("\n")
+class GamePlay:
 
-    # lines = wordlist.readlines()
+    def __init__(self):
+        self.wordlist = WordList()
+        self.random_word = RandomWord()
 
-    # for index, line in enumerate(lines):
-    #     print("Line {}: {}".format(index, line.strip()))
-
-
-    # line = wordlist.readline()
-    # cnt=1
-    # while line:
-    #     print("line {}: {}".format(cnt, line.strip()))
-    #     line = wordlist.readline()
-    #     cnt += 1
-    # word=wordlist.readlines(4)
-    # print (word)
-
+    def game_play(self):
+        pass
+if __name__ == '__main__':
+    play_hangman = GamePlay()
+    # play_hangman.game_play()
